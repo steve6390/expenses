4b15449ddefc06821de16fc746de7a810c115fc6
srking <sk@metrokings.com>
2017-08-26 22:11:38 -0700

    More unit tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,7 +7,8 @@ main = defaultMain unitTests
 
 unitTests = 
     testGroup "Unit Tests"
-    [ gpm1, gpm2 ]
+    [ gpm1, gpm2, gpm3  ]
 
-gpm1 = testCase "getPrevMonth test 1" $ assertEqual [] (2017,4) (getPrevMonth (2017,5,1))
-gpm2 = testCase "getPrevMonth test 2" $ assertEqual [] (2017,12) (getPrevMonth (2017,11,1))
+gpm1 = testCase "getPrevMonth test 1" $ assertEqual [] (2017,4) (getPrevMonth (2017,5,31))
+gpm2 = testCase "getPrevMonth test 2" $ assertEqual [] (2017,10) (getPrevMonth (2017,11,1))
+gpm3 = testCase "getPrevMonth test 3" $ assertEqual [] (2016,12) (getPrevMonth (2017,1,14))
