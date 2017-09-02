5840f7d5c64be0416ef70b1912e8bb0abd6a5fc1
srking <sk@metrokings.com>
2017-09-01 22:24:13 -0700

    extracts a list of debit values from the lines

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,20 @@ import System.IO
 import Data.Time.Clock
 import Data.Time.Calendar
 import DateFuncs
+import Text.Regex.PCRE
+
+-- The debit amount is the value before the word 'debit'
+-- Two digits after the decimal are always present
+getDebit :: String -> Float
+getDebit line = do
+    let m = line =~ "\"(\\d+\\.\\d\\d)\"\\s*,\\s*\"debit\"" :: [[String]]
+    -- debit value emerges from this regex looking like this:
+    -- [["\"4.91\",\"debit\"","4.91"]]
+    -- This is a list of lists.  Each list item is a list of match string followed by groups
+    -- To extract 4.91 we have to do some stupid gyrations compare to other languages
+    let debit = head (tail (head m))
+    -- now debug is just a string "4.91"
+    read debit :: Float
 
 main :: IO ()
 main = do
@@ -17,4 +31,7 @@ main = do
     let linesWithMonth = getLinesWithMonth lastMonth transactions
     putStrLn $ "transactions last month: " ++ show (length linesWithMonth)
     putStrLn $ unlines linesWithMonth
+    let allDebits = map getDebit linesWithMonth
+    print allDebits
+    
 
